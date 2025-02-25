fbb96993ee59ead91b337076c06ac1561cb1aabb
ilya9293 <ilya9293@ukr.net>
2025-02-25 20:17:56 +0200

    increase font-sizes, remove toggle sliders

--- app/js/courseToggleSlider.js
+++ app/js/courseToggleSlider.js
@@ -1,31 +1,31 @@
-const courseProgramme = document.querySelector("#course-programme");
+// const courseProgramme = document.querySelector("#course-programme");
 
-document.addEventListener("DOMContentLoaded", () => {
-  [...courseProgramme.children].forEach((item) => {
-    const heightModule = item.firstElementChild.scrollHeight;
-    const lastChild = item.lastElementChild;
-    const computedStyle = window.getComputedStyle(lastChild);
-    const paddingBottom = parseFloat(computedStyle.paddingBottom) || 0;
+// document.addEventListener("DOMContentLoaded", () => {
+//   [...courseProgramme.children].forEach((item) => {
+//     const heightModule = item.firstElementChild.scrollHeight;
+//     const lastChild = item.lastElementChild;
+//     const computedStyle = window.getComputedStyle(lastChild);
+//     const paddingBottom = parseFloat(computedStyle.paddingBottom) || 0;
 
-    if (heightModule > lastChild.scrollHeight - paddingBottom) {
-      lastChild.lastElementChild.classList.add("hide");
-    }
-    lastChild.style.height = heightModule + "px";
-  });
-});
+//     if (heightModule > lastChild.scrollHeight - paddingBottom) {
+//       lastChild.lastElementChild.classList.add("hide");
+//     }
+//     lastChild.style.height = heightModule + "px";
+//   });
+// });
 
-courseProgramme.addEventListener("click", (e) => {
-  if (e.target.classList.contains("box-slider")) {
-    const activeClass = "slider-active";
-    const courseWraper = e.target.parentElement;
-    const courseModule = courseWraper.parentElement.firstElementChild.firstElementChild;
+// courseProgramme.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("box-slider")) {
+//     const activeClass = "slider-active";
+//     const courseWraper = e.target.parentElement;
+//     const courseModule = courseWraper.parentElement.firstElementChild.firstElementChild;
 
-    if (courseWraper.classList.contains(activeClass)) {
-      courseWraper.classList.remove(activeClass);
-      courseWraper.style.height = courseModule.scrollHeight + "px";
-    } else {
-      courseWraper.classList.add(activeClass);
-      courseWraper.style.height = courseWraper.scrollHeight + "px";
-    }
-  }
-});
+//     if (courseWraper.classList.contains(activeClass)) {
+//       courseWraper.classList.remove(activeClass);
+//       courseWraper.style.height = courseModule.scrollHeight + "px";
+//     } else {
+//       courseWraper.classList.add(activeClass);
+//       courseWraper.style.height = courseWraper.scrollHeight + "px";
+//     }
+//   }
+// });
